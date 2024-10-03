2d567fc4a78b5207ab0dc0391d4b485389f277af
ncarrier757 <50429121+ncarrier757@users.noreply.github.com>
2024-10-03 16:37:45 -0700

    Update help.py

--- help.py
+++ help.py
@@ -9,7 +9,7 @@ def p(text):
     print(text)
     print('-' * 200)
 
-def get_mgmt_chain(data, id_col='fid', employee='name', manager='manager', ceo_fid=181):
+def get_mgmt_chain(data, id_col='fid', employee='name', manager='manager', ceo_fid=1):
 
     data.loc[data[id_col] == ceo_fid, manager] = np.nan
     data[[employee, manager]] = data[[employee, manager]].replace(r' \(On Leave\)', '', regex=True)
@@ -34,19 +34,6 @@ def label_graph(data, x, y, label, alpha=1, size='x-small'):
         record = data.loc[i]
         ax = plt.gca()
         ax.text(record[x], record[y], record[label], size=size, alpha=alpha)
-
-geo_diffs = {'Austin': -.08,
-             'Boston': -.01,
-             'Charlotte': -.09,
-             'Chicago': -.05,  # was -.06
-             'Dallas': -.06,
-             'Denver': -.06,  # was -.05
-             'Los Angeles': .01,
-             'New York': .04,  # was .05
-             'Phoenix': -.08,  # was -.12, then -.11
-             'Portland': -.05,  # was -.08, then -.07
-             'San Francisco': .07,  # was .08
-             'Seattle': 0}
 
 def test():
     while True:
